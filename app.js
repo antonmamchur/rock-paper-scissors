dfd760d230d329317db75a7501a03af7f05f952f
antonmamchur <mamchuranton@gmail.com>
2022-06-06 11:27:31 +0600

    fix: code refactoring

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const controls = document.querySelector(".controls");
+const round = document.querySelector("#inc");
+
 controls.addEventListener("click", game);
 
-let counter = 0;
+let roundNumber = 0;
 let playerScore = 0;
 let computerScore = 0;
 
 function computersPlay() {
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  return randomNumber === 1
+  let random = Math.floor(Math.random() * 3 + 1);
+  return random === 1
     ? "rock"
-    : randomNumber === 2
+    : random === 2
     ? "paper"
     : "scissors";
 }
@@ -17,69 +19,68 @@ function computersPlay() {
 function playRound(playersChoice, computersChoice) {
   const result = document.querySelector(".result");
   let player = playersChoice;
-  let computer = computersChoice.toLowerCase();
+  let computer = computersChoice;
+
+  //round counter
+    round.textContent = `Current round: ${++roundNumber}`;
+
+    //outcome of one round
   if (player === computer) {
-    return [
-      (result.textContent = `Result: It's a tie! You both picked ${player.toUpperCase()}`),
-      null,
-    ];
+      result.textContent = `Result: It's a tie! You both picked ${player.toUpperCase()}`
+      computerScore++
+      playerScore++
+
   } else if (
     (player === "rock" && computer === "scissors") ||
     (player === "paper" && computer === "rock") ||
     (player === "scissors" && computer === "paper")
   ) {
-    return [
-      (result.textContent = `Result: You've won! ${player.toUpperCase()} beats ${computer.toUpperCase()}`),
-      true,
-    ];
+      result.textContent = `Result: You've won! ${player.toUpperCase()} beats ${computer.toUpperCase()}`
+      playerScore++
+ 
   } else {
-    return [
-      (result.textContent = `Result: You've lost! ${player.toUpperCase()} loses to ${computer.toUpperCase()}`),
-      false,
-    ];
+      result.textContent = `Result: You've lost! ${player.toUpperCase()} loses to ${computer.toUpperCase()}`
+      computerScore++
+
   }
 }
 
 function game(e) {
-  let round = document.querySelector("#inc");
-  let outputContainer = document.querySelector(".output");
-  round.textContent = `Current round: ${++counter}`;
-
   let playersSelection = e.target.value;
   let computerSelection = computersPlay();
-  let output = playRound(playersSelection, computerSelection);
+playRound(playersSelection, computerSelection);
+gameOver()
 
-  if (output[1] === true) {
-    output[0];
-    playerScore += 1;
-  } else if (output[1] === false) {
-    output[0];
-    computerScore += 1;
-  } else {
-    output[0];
-    computerScore += 1;
-    playerScore += 1;
-  }
 
-  let winner = document.createElement("div");
-  winner.classList.add("winner");
-
-  if (counter == 5) {
-    if (playerScore > computerScore) {
-      outputContainer.appendChild(winner);
-      winner.textContent = "You won!";
-    } else if (computerScore === playerScore) {
-      outputContainer.appendChild(winner);
-      winner.textContent = "Tie!";
-    } else {
-      outputContainer.appendChild(winner);
-      winner.textContent = "You lost!";
-    }
-    counter = 0;
-  } else if (counter == 1) {
-    let winner = document.querySelector(".winner");
-    if (winner) {
-      outputContainer.removeChild(winner);
-    }
-  }
 }
+
+
+function gameOver() {
+  let winner = document.querySelector(".winner");
+
+  // game result
+  if (roundNumber == 5) {
+    if (playerScore > computerScore) {
+      winner.textContent = "You won!";
+      
+    } else if (computerScore === playerScore) {
+      winner.textContent = "Tie!";
+
+    } else {
+      winner.textContent = "You lost!";
+
+
+    }
+    roundNumber = 0;
+    playerScore = 0
+    computerScore = 0
+    
+  } 
+
+  // refreshes the game result
+  if( roundNumber === 1 ) {
+    winner.textContent = ''
+  }
+
+}
+
